6cc39ffb3bc1b32478942c850c2450db179afa13
Seung Kyem Kim <seung.kim1998@gmail.com>
2022-04-30 13:09:32 -0700

    made changes to the giphy api key

--- script.js
+++ script.js
@@ -40,19 +40,33 @@ generateBtn.addEventListener('click', function() {
         getImages(attr);
 })
 
+var display = document.querySelector(".test");
+var img = document.querySelector("#myImg");
 
+let key = "PdkNRVt59jQTuyCn6UBmpXpacH1ulMhA";
+let urlMeme =
+  "https://api.giphy.com/v1/gifs/search?api_key=PdkNRVt59jQTuyCn6UBmpXpacH1ulMhA&q=gif&limit=25&offset=0&rating=g&lang=en";
 
-// let key = "PdkNRVt59jQTuyCn6UBmpXpacH1ulMhA";
-// let urlMeme = "https://api.giphy.com/v1/gifs/search?api_key=PdkNRVt59jQTuyCn6UBmpXpacH1ulMhA&q=cat&limit=25&offset=0&rating=g&lang=en";
+fetch(urlMeme)
+  .then((response) => {
+    console.log(response, "response");
+    return response.json();
+  })
+  .then((data) => {
+    data.data.forEach((item) => {
+      console.log(item);
+      display.innerHTML += `<img src=${item.images.original.url}/><br>`;
+    });
+    console.log(data, "data");
 
-// fetch(urlMeme)
-// .then(response =>{
-//     console.log(response.status)
-//     return response.json();
-// })
-// .then(data=>{
-//     console.log(data)
-// })
-// .catch(console.err);
-
-
+    async function pullData() {
+      var final = await fetch(urlMeme);
+      console.log(final);
+      var data = await final.json();
+      data.data.forEach((item) => {
+        display.innerHTML += `<img src=${item.images.original.url}/><br>`;
+      });
+      console.log(data);
+    }
+    pullData();
+  });
