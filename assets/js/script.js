28e714e162e994f3c60a9cb5bb83dec599b825a1
josh jankowski <joshjankowski@gmail.com>
2022-05-03 23:01:41 -0700

    added dragable text via javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,6 +62,68 @@ searchMeme.addEventListener("click", function () {
   // generateSelection();
 });
 
+let inputTextBox = document.querySelector("#value")
+let submitText = document.querySelector("#submit-text")
+let topText = document.querySelector(".top")
+let bottomText = document.querySelector(".bottom")
+var ele = document.getElementsByName('text');
+
+submitText.addEventListener("click", function(){
+let inPutText = inputTextBox.value;
+for(i = 0; i < ele.length; i++) {
+  // Select Top Text
+  if(ele[0].checked) {
+    topText.textContent = inPutText;
+    } else {
+      // Select Bottom Text
+    bottomText.textContent = inPutText;
+    }
+}
+
+})
+
+// Code to move text moveable
+
+let selected = null, // Object of the element to be moved
+  x_pos = 0,
+  y_pos = 0, // Stores x & y coordinates of the mouse pointer
+  x_elem = 0,
+  y_elem = 0; // Stores top, left values (edge) of the element
+
+// Will be called when user starts dragging an element
+function _drag_init(elem) {
+  // Store the object of the element which needs to be moved
+  selected = elem;
+  x_elem = x_pos - selected.offsetLeft;
+  y_elem = y_pos - selected.offsetTop;
+}
+
+// Will be called when user dragging an element
+function _move_elem(e) {
+  x_pos = document.all ? window.event.clientX : e.pageX;
+  y_pos = document.all ? window.event.clientY : e.pageY;
+  if (selected !== null) {
+    selected.style.left = (x_pos - x_elem) + 'px';
+    selected.style.top = (y_pos - y_elem) + 'px';
+  }
+}
+
+// Destroy the object when we are done
+function _destroy() {
+  selected = null;
+}
+
+// Bind the functions...
+document.querySelector(".moveable").onmousedown = function() {
+  _drag_init(this);
+  return false;
+};
+
+document.onmousemove = _move_elem;
+document.onmouseup = _destroy;
+
+
+
 
 
     
